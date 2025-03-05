c3d1561c91ee52ea228b090f7fc24076eebaa545
rizkyrachman00 <rachman.ti2021@students.amikom.ac.id>
2025-03-06 01:58:31 +0700

    feat: string function

--- string.go
+++ string.go
@@ -5,4 +5,9 @@ import "fmt"
 func main() {
 	fmt.Println("Rizky")
 	fmt.Println("Rizky Rachman")
+	fmt.Println("Rizky Rachman")
+
+	fmt.Println(len("Rizky"))
+	fmt.Println("Rizky Rachman"[0])
+	fmt.Println("Rizky Rachman"[3])
 }
